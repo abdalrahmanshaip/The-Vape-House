2a38c10cc25f5b4cfed9389f139602e26c27251d
abdalrahman <abdalrahmanshaip@gmail.com>
2024-09-14 23:18:41 +0300

    add related products to details page

--- app/(pages)/collections/disposable/[id]/page.tsx
+++ app/(pages)/collections/disposable/[id]/page.tsx
@@ -1,5 +1,6 @@
-import { getDispoById } from '@/_actions/disposableAction'
+import { getAllDesposable, getDispoById } from '@/_actions/disposableAction'
 import Quantity from '@/app/components/Quantity'
+import RelatedProducts from '@/app/components/RelatedProducts'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import Image from 'next/image'
@@ -14,47 +15,54 @@ const ViewDetailsDisposable = async ({
   searchParams: { flavor: string }
 }) => {
   const dataItem = await getDispoById(params.id)
+  const { data } = await getAllDesposable()
   const imgSrc = `data:${dataItem?.data?.img.contentType};base64,${dataItem.data?.img.data}`
   const flavors = dataItem.data?.disposable.flavor.split(',')
   return (
-    <div className='mt-10  justify-center flex gap-x-10 w-full md:flex-row flex-col'>
-      <div className='left w-full flex md:justify-end'>
-        <Image
-          src={imgSrc}
-          alt={dataItem.data?.disposable.productName}
-          width={500}
-          height={500}
-        />
-      </div>
-      <div className='right w-full'>
-        <h2 className='text-2xl font-bold'>
-          {dataItem.data?.disposable.productName}
-        </h2>
-        <p className='text-lg font-bold '>
-          LE {dataItem.data?.disposable.price}.00
-        </p>
-        <p className='mt-5 text-sm'>Flavors: {searchParams.flavor}</p>
-        <div className='mt-2 space-x-4'>
-          {flavors.map((item: string) => {
-            return (
-              <Button
-                key={item}
-                variant={'ghost'}
-                className={`p-2 border-gray-300 border ${
-                  searchParams.flavor === item && 'border-black'
-                }`}
-                asChild
-              >
-                <Link href={`?flavor=${item}`}>{item}</Link>
-              </Button>
-            )
-          })}
+    <div>
+      <div className='mt-10  justify-center flex gap-x-10 w-full md:flex-row flex-col'>
+        <div className='left w-full flex md:justify-end'>
+          <Image
+            src={imgSrc}
+            alt={dataItem.data?.disposable.productName}
+            width={500}
+            height={500}
+          />
         </div>
-        <div>
-          <h3 className='text-sm mt-10 text-muted-foreground'>Quantity:</h3>
-          <Quantity quantityOfItem={dataItem.data?.disposable} />
+        <div className='right w-full'>
+          <h2 className='text-2xl font-bold'>
+            {dataItem.data?.disposable.productName}
+          </h2>
+          <p className='text-lg font-bold '>
+            LE {dataItem.data?.disposable.price}.00
+          </p>
+          <p className='mt-5 text-sm'>Flavors: {searchParams.flavor}</p>
+          <div className='mt-2 space-x-4'>
+            {flavors.map((item: string) => {
+              return (
+                <Button
+                  key={item}
+                  variant={'ghost'}
+                  className={`p-2 border-gray-300 border ${
+                    searchParams.flavor === item && 'border-black'
+                  }`}
+                  asChild
+                >
+                  <Link href={`?flavor=${item}`}>{item}</Link>
+                </Button>
+              )
+            })}
+          </div>
+          <div>
+            <h3 className='text-sm mt-10 text-muted-foreground'>Quantity:</h3>
+            <Quantity quantityOfItem={dataItem.data?.disposable} />
+          </div>
         </div>
       </div>
+        <div className='Related-Products my-10'>
+          <h3 className='border-t text-center font-bold text-xl py-5'>Related Products</h3>
+          <RelatedProducts data={data?.disposables}/>
+        </div>
     </div>
   )
 }
---
+++ app/components/RelatedProducts.tsx
@@ -0,0 +1,60 @@
+import { getAllDesposable } from '@/_actions/disposableAction'
+import { Card, CardContent } from '@/components/ui/card'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
+import { TypeDispo } from '@/Types'
+import Image from 'next/image'
+
+const RelatedProducts = ({ data }: { data: any}) => {
+  return (
+    <Carousel
+      opts={{
+        align: 'start',
+      }}
+      className='w-full'
+    >
+      <CarouselContent>
+        {data.map((item: TypeDispo, index: number) => {
+          const imgSrc = `data:${item.img.contentType};base64,${item.img.data}`
+          return (
+            <CarouselItem
+              key={index}
+              className='md:basis-1/2 lg:basis-1/4'
+            >
+              <div className='p-1'>
+                <Card>
+                  <CardContent className='flex flex-col  items-center justify-center p-0'>
+                    <Image
+                      className='w-full h-52 object-contain'
+                      src={imgSrc}
+                      alt={item.productName}
+                      width={400} // You can adjust the width and height as needed
+                      height={300}
+                    />
+                  </CardContent>
+                </Card>
+                <div className='title mt-2'>
+                  <p className='text-start  capitalize'>{item.productName}</p>
+                  <p className='text-start font-bold'>LE {item.price}.00</p>
+                </div>
+              </div>
+            </CarouselItem>
+          )
+        })}
+      </CarouselContent>
+      <CarouselPrevious className='hidden lg:flex' />
+      <CarouselNext className='hidden lg:flex' />
+      <div className=' space-x-4 absolute left-1/2 -bottom-5  lg:hidden '>
+        <CarouselPrevious />
+        <CarouselNext />
+      </div>
+    </Carousel>
+  )
+}
+
+export default RelatedProducts
