8996ecd0213be0f251b4b57d5d196471bbeed19d
abdalrahman <abdalrahmanshaip@gmail.com>
2024-09-21 08:08:12 +0300

    in top dispo add see all to navigate to disposables page

--- app/components/TopDisposables.tsx
+++ app/components/TopDisposables.tsx
@@ -1,11 +1,18 @@
+import Link from 'next/link'
 import CarouselTopItem from '../shared/CarouselTopItem'
+import { Button } from '@/components/ui/button'
 
 const TopDisposables = () => {
   return (
     <div className='container mx-auto my-7 px-5'>
       <div className='title flex mb-5 items-center justify-between'>
         <h2 className='text-2xl font-bold '>Top Disposables</h2>
-        <p className=''>see all</p>
+        <Button
+          asChild
+          variant={'ghost'}
+        >
+          <Link href={'/collections/disposable'}>see all </Link>
+        </Button>
       </div>
       <CarouselTopItem />
     </div>
